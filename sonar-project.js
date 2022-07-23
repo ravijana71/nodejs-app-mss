3e76276b56f7d7d015619ba8276706c30dff50fc
ravijana71 <100750842+ravijana71@users.noreply.github.com>
2022-07-23 17:52:40 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://http://43.204.227.159:9000/',
+  serverUrl: 'http://43.204.227.159:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
